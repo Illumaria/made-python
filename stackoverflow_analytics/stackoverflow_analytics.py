2a96b6fe61656c909909e4b38fceea1860f1fc52
Illumaria <astankov.dmitry@gmail.com>
2020-12-10 19:13:03 +0300

    Update source files

--- stackoverflow_analytics/stackoverflow_analytics.py
+++ stackoverflow_analytics/stackoverflow_analytics.py
@@ -5,11 +5,22 @@ from argparse import ArgumentTypeError
 from argparse import FileType
 from collections import defaultdict
 from io import TextIOWrapper
-# import re
+import logging
+from lxml import etree
+import re
 import sys
 
-DEFAULT_QUESTIONS_FPATH = "questions.xml"
+APPLICATION_NAME = "stackoverflow_analytics"
+DEFAULT_APP_HANDLER_FPATH = "stackoverflow_analytics.log"
+DEFAULT_WARN_HANDLER_FPATH = "stackoverflow_analytics.warn"
+DEFAULT_QUERIES_FPATH = "queries_sample.csv"
+DEFAULT_SMALL_QUESTIONS_FPATH = "questions_sample.xml"
+DEFAULT_BIG_QUESTIONS_FPATH = "stackoverflow_posts_sample.xml"
 DEFAULT_STOP_WORDS_FPATH = "stop_words_in_koi8r.txt"
+
+
+
+logger = logging.getLogger(APPLICATION_NAME)
 
 
 class EncodedFileType(FileType):
@@ -35,43 +46,90 @@ class EncodedFileType(FileType):
             raise ArgumentTypeError(f"Can't open '{string}': {error}.")
 
 
-def process_arguments(questions_filepath, stopwords_filepath, query_file):
-    pass
+def load_questions(questions_filepath):
+    with open(DEFAULT_BIG_QUESTIONS_FPATH, "r") as fin:
+        content = fin.readlines()
+        print(len(content))
+        for line in content:
+            root = etree.fromstring(line, parser=etree.XMLParser())
+            if int(root.get("PostTypeId")) == 1:
+                score = int(root.get("Score"))
+                year = int(root.get("CreationDate").split('-')[0])
+                title = root.get("Title")
+                title_tokens = re.findall("\w+", title.lower())
+                print(year, score, title_tokens)
+
+
+def load_stop_words(fin) -> set:
+    """Load stop words from file"""
+    # with open(filepath, "r") as fin:
+        # return fin.read().splitlines()
+    return set(fin.read().splitlines())
+
+
+def process_arguments(questions_filepath, stopwords_file, query_filepath):
+    questions = load_questions(questions_filepath)
+    stop_words = load_stop_words(stopwords_file)
 
 
 def callback_parser(arguments):
     """Callback function"""
     return process_arguments(arguments.questions_filepath,
-                             arguments.stopwords_filepath,
-                             arguments.query_file)
+                             arguments.stopwords_file,
+                             arguments.query_filepath)
 
 
 def setup_parser(parser):
     """The function to setup parser arguments"""
     parser.add_argument(
         "--questions",
-        default=DEFAULT_QUESTIONS_FPATH,
+        default=DEFAULT_SMALL_QUESTIONS_FPATH,
         dest="questions_filepath",
         help="path to questions in xml format",
     )
     parser.add_argument(
         "--stop-words",
         default=DEFAULT_STOP_WORDS_FPATH,
-        dest="stopwords_filepath",
-        type=EncodedFileType("r", encoding="koi8r"),
-        # default=TextIOWrapper(sys.stdin.buffer, encoding="koi8r"),
+        dest="stopwords_file",
+        type=EncodedFileType("r", encoding="koi8-r"),
         help="path to stop-words file in text format",
     )
     parser.add_argument(
         "--queries",
-        dest="query_file",
+        default=DEFAULT_QUERIES_FPATH,
+        dest="query_filepath",
         help="query file in csv format to get queries for analytics",
     )
     parser.set_defaults(callback=callback_parser)
 
 
+def setup_logging():
+    """The function to setup logger"""
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        fmt="%(levelname)s: %(message)s",
+        # datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    app_handler = logging.FileHandler(
+        filename=DEFAULT_APP_HANDLER_FPATH,
+    )
+    app_handler.setLevel(logging.DEBUG)
+    app_handler.setFormatter(formatter)
+
+    warn_handler = logging.FileHandler(
+        filename=DEFAULT_WARN_HANDLER_FPATH,
+    )
+    warn_handler.setLevel(logging.WARNING)
+    warn_handler.setFormatter(formatter)
+
+    logger.addHandler(app_handler)
+    logger.addHandler(warn_handler)
+
+
 def main():
     """Main module function"""
+    setup_logging()
     parser = ArgumentParser(
         prog="stackoverflow-analytics",
         description="A tool to query stackoverflow website.",
